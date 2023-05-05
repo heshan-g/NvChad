da774ede6590f7b89d2eb9bdd33047d6686b106d
Heshan G <heshan@Heshans-MacBook-Pro.local>
2023-05-05 14:23:10 +0530

    Add treesitter plugin configs for web dev

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,21 @@
+local plugins = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+      },
+    },
+  },
+}
+return plugins

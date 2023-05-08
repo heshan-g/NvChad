64717900bba4579cf3bd4fbe267c21ece35cbf70
Heshan <heshan.ua@gmail.com>
2023-05-08 23:54:22 +0530

    Update prettier configs

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -4,9 +4,15 @@ local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
 null_ls.setup({
-   debug = true,
-   sources = {
-    formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" }}),
+  debug = true,
+  sources = {
+    formatting.prettier.with({
+      extra_args = {
+        "--single-quote",
+        "--jsx-single-quote",
+        "--end-of-line lf"
+      }
+    }),
     formatting.stylua,
 
     lint.shellcheck,
